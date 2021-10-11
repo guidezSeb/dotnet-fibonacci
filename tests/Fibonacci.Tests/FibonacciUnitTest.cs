39ce63ced3a3932b753a4673070c58d072ddd27d
guidezSeb <sebastien.guidez@lacatholille.fr>
2021-10-11 17:53:57 +0200

    test fix?

--- tests/Fibonacci.Tests/FibonacciUnitTest.cs
+++ tests/Fibonacci.Tests/FibonacciUnitTest.cs
@@ -17,6 +17,7 @@ namespace Fibonacci.Tests
             builder.UseInMemoryDatabase(dataBaseName);
             var options = builder.Options;
             var fibonacciDataContext = new FibonacciDataContext(options);
+            
             await fibonacciDataContext.Database.EnsureCreatedAsync(); 
             var result = await new Compute(fibonacciDataContext).ExecuteAsync(new[] {"4"});
             Assert.Equal(3, result[0]);
