a503b83b598da63ae2de7eb51a58842f0ae903ea
guidezSeb <sebastien.guidez@lacatholille.fr>
2021-10-11 16:35:15 +0200

    test + sql

--- tests/Fibonacci.Tests/FibonacciUnitTest.cs
+++ tests/Fibonacci.Tests/FibonacciUnitTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace Fibonacci.Tests
@@ -6,9 +8,17 @@ namespace Fibonacci.Tests
 
     public class FibonacciUnitTest
     {
+  
+
         [Fact]
         public async Task Execute44ShouldRetrun701408733() {
-            var result = await Compute.ExecuteAsync(new[] {"4"});
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
+            var dataBaseName = Guid.NewGuid().ToString();
+            builder.UseInMemoryDatabase(dataBaseName);
+            var options = builder.Options;
+            var fibonacciDataContext = new FibonacciDataContext(options);
+            await fibonacciDataContext.Database.EnsureCreatedAsync(); 
+            var result = await new Compute(fibonacciDataContext).ExecuteAsync(new[] {"4"});
             Assert.Equal(3, result[0]);
         }
     }
