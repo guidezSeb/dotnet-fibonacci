fa09bff40be76fa51443e217912bdd9f0244ea2c
guidezSeb <sebastien.guidez@lacatholille.fr>
2021-10-12 08:33:26 +0200

    change test

--- tests/Fibonacci.Tests/FibonacciUnitTest.cs
+++ tests/Fibonacci.Tests/FibonacciUnitTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -8,19 +9,20 @@ namespace Fibonacci.Tests
 
     public class FibonacciUnitTest
     {
-  
-
         [Fact]
-        public async Task Execute44ShouldRetrun701408733() {
-            var builder = new DbContextOptionsBuilder<FibonacciDataContext>();
-            var dataBaseName = Guid.NewGuid().ToString();
-            builder.UseInMemoryDatabase(dataBaseName);
-            var options = builder.Options;
-            var fibonacciDataContext = new FibonacciDataContext(options);
-            
+        public async Task Execute6ShouldRetrun8()
+        {
+            var builder = new DbContextOptionsBuilder<FibonacciDataContext>(); 
+            var dataBaseName = Guid.NewGuid().ToString();  
+            builder.UseInMemoryDatabase(dataBaseName);    
+            var options = builder.Options;  
+            var fibonacciDataContext = new FibonacciDataContext(options);  
             await fibonacciDataContext.Database.EnsureCreatedAsync(); 
-            var result = await new Compute(fibonacciDataContext).ExecuteAsync(new[] {"4"});
-            Assert.Equal(3, result[0]);
+
+            
+            var result =  await new Fibonacci.Compute(fibonacciDataContext).ExecuteAsync(new[] {"6"});
+            Assert.Equal(1, result.Count);
+            Assert.Equal(8, result[0]);
         }
     }
 }
